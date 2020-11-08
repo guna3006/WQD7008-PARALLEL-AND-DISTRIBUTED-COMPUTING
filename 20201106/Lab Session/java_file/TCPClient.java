74d78070ff6770ebfae67be061457abb7cd6b435
Gunasegarran Magadevan <36212351+guna3006@users.noreply.github.com>
2020-11-08 21:13:46 +0800

    Quiz Commit

---
+++ 20201106/Lab Session/java_file/TCPClient.java
@@ -0,0 +1,35 @@
+// TCPClient.java
+// A client program implementing TCP socket
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+	public static void main (String args[])
+	{// arguments supply message and hostname of destination
+        Socket s = null;
+    	try{
+	      int serverPort = 10100;
+              String ip = "localhost";
+              String data = "Hello, How are you? using 10101";
+
+	      s = new Socket(ip, serverPort);
+	      DataInputStream input = new DataInputStream( s.getInputStream());
+	      DataOutputStream output = new DataOutputStream( s.getOutputStream());
+	      output.writeUTF(data); // UTF is a string encoding
+
+	      System.out.println("Received: "+ data) ;
+	    }
+	    catch (UnknownHostException e){
+	    	System.out.println("Sock:"+e.getMessage());}
+	    catch (EOFException e){
+	    	System.out.println("EOF:"+e.getMessage()); }
+	    catch (IOException e){
+	    	System.out.println("IO:"+e.getMessage());}
+    	finally {
+      		if(s!=null)
+      			try {s.close();
+      			}
+      			catch (IOException e) {/*close failed*/}
+    }
+  }
+}
